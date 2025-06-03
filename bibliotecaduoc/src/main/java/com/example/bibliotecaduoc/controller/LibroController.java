ec8dbd7ca07cd70d75f2369995fcf0fbb056ad92
22lobos <cr.loboso@duocuc.cl>
2025-06-03 01:23:36 -0400

    Tu mensaje

--- bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -6,12 +6,27 @@ import com.example.bibliotecaduoc.service.LibroServise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/api/v1/libros")
 public class LibroController {
 
+    @Autowired
+    private LibroServise libroServise;
+    @GetMapping
+
+    public List<Libro> listarLibros() {
+        return libroServise.obtenerLibros();
+        
+        
+    }
     
+
+
+
     
 }
