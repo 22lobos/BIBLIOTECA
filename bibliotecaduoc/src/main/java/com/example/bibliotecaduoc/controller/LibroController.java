71b3cf410499528f97bc7ea33eccf92c4b655551
22lobos <cr.loboso@duocuc.cl>
2025-06-03 01:21:28 -0400

    modificado

--- bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,5 +1,17 @@
 package com.example.bibliotecaduoc.controller;
 
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroServise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/libros")
 public class LibroController {
+
+    
     
 }
