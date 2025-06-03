d51455b987bfb5ed4e0feddafca5cb082d29394a
22lobos <cr.loboso@duocuc.cl>
2025-06-03 01:55:38 -0400

    modificado

--- bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -6,11 +6,18 @@ import com.example.bibliotecaduoc.service.LibroServise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
 @RestController
+
+
 @RequestMapping("/api/v1/libros")
 public class LibroController {
 
@@ -21,7 +28,19 @@ public class LibroController {
         return libroServise.obtenerLibros();
 
 
-    }    @GetMapping("/{id}")
+    }  
+      @PostMapping
+    public String agregarLibro( @RequestBody  Libro libro ) {
+        
+        return libroServise.saveLibro(libro).toString();
+
+        
+    }
+    @GetMapping("/{id}")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     public Libro agregaLibro(@RequestBody Libro libro) {
         return libroServise.saveLibro(libro);
         
