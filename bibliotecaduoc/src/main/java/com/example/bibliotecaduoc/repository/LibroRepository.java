413c117dc468d03432fa05a69cda66e1182b5ac6
22lobos <cr.loboso@duocuc.cl>
2025-06-03 00:30:26 -0400

    repository modificado

--- bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -1,5 +1,21 @@
 package com.example.bibliotecaduoc.repository;
+import org.springframework.stereotype.Repository;
+import com.example.bibliotecaduoc.model.Libro;
+import java.util.List;
+import java.util.ArrayList;
 
+
+
+
+@Repository
 public class LibroRepository {
+
+private List<Libro>  listarLibros = new ArrayList<>();
+
+public List<Libro> obteLibros() {
+    return listarLibros;
+    }
+
+
     
 }
